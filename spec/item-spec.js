b6c3728573808234d31e8aa101d08b166532c39e
carolineF <1138483832@qq.com>
2015-07-31 17:31:44 +0800

    添加item类的单元测试

---
+++ spec/item-spec.js
@@ -0,0 +1,29 @@
+var Item = require('../main/model/item.js');
+var allItem = require('../main/model/fixtures.js');
+
+describe('Item', function() {
+  describe('.find()', function() {
+    it('should find a Item', function() {
+      spyOn(allItem, 'loadAllItems').and.returnValue([
+        new Item('ITEM000001', '雪碧', '瓶', 3.00),
+        new Item('ITEM000002', '苹果', '斤', 5.50)
+        ]);
+
+        expect(Item.find('ITEM000001')).toEqual(new Item('ITEM000001', '雪碧', '瓶', 3.00));
+    });
+  });
+
+  describe('.all()', function() {
+    it('can return all item', function() {
+      spyOn(allItem, 'loadAllItems').and.returnValue([
+        new Item('ITEM000001', '雪碧', '瓶', 3.00),
+        new Item('ITEM000002', '苹果', '斤', 5.50)
+        ]);
+
+        expect(Item.all()).toEqual([
+          new Item('ITEM000001', '雪碧', '瓶', 3.00),
+          new Item('ITEM000002', '苹果', '斤', 5.50)
+          ]);
+    });
+  });
+});
